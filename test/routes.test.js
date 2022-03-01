1631023315b367c4ff041583f3038768a73ccec1
Bruno Krugel <brunokrugel@gmail.com>
2022-03-01 04:12:47 -0300

    giveup

--- test/routes.test.js
+++ test/routes.test.js
@@ -1,7 +1,5 @@
 const request = require("supertest");
 const app = require("../app");
-const sinon = require('sinon');
-const axios = require('axios');
 
 test('Ping', async () => {
     await request(app).get("/api/ping").expect(200, {
